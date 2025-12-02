1fe3b5064b6a22158a80e61f126dab7e055f00bb
KiryakovMaxim <kiryakovm96@gmail.com>
2025-12-03 02:09:39 +0300

    feat lesson 21: Added trigger

--- lesson-21-code-review/server/src/main/kotlin/com/prike/Main.kt
+++ lesson-21-code-review/server/src/main/kotlin/com/prike/Main.kt
@@ -44,6 +44,7 @@ fun main(args: Array<String>) {
         aiConfig = config.ai,
         defaultTemperature = config.ai.temperature,
         defaultMaxTokens = config.ai.maxTokens
+    )
     
     // 2. PromptBuilder для формирования промптов с контекстом
     val promptBuilder = PromptBuilder()
